dbcfaf931b2e457f770aae8275aa95fdf519bb96
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-14 11:24:37 +0530

    BaseActivity created

---
+++ app/src/main/java/com/chat/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.chat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by techteam on 14/10/16.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+  }
+}
