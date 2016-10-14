02a5b1bb502b22b2602abe891e36534a04458696
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-14 11:32:58 +0530

    logging

--- app/src/main/java/com/chat/BaseActivity.java
+++ app/src/main/java/com/chat/BaseActivity.java
@@ -3,6 +3,7 @@ package com.chat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.novoda.notils.logger.simple.Log;
 
 /**
  * Created by techteam on 14/10/16.
@@ -13,6 +14,6 @@ public class BaseActivity extends AppCompatActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
+    Log.setShowLogs(BuildConfig.DEBUG);
   }
 }
