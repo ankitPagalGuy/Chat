b11794bcf3011cfc733d5c19f4c690002d9ba273
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-17 21:48:25 +0530

    login code written , to be tested

--- app/src/main/java/com/chat/login/LoginActivity.java
+++ app/src/main/java/com/chat/login/LoginActivity.java
@@ -1,27 +1,111 @@
 package com.chat.login;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 import butterknife.Bind;
+import butterknife.OnClick;
 import com.chat.BaseActivity;
 import com.chat.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * Created by techteam on 16/10/16.
  */
 
-public class LoginActivity  extends BaseActivity{
+public class LoginActivity  extends BaseActivity implements GoogleApiClient.OnConnectionFailedListener{
 
- @Bind(R.id.sign_in_button) View signInButton;
+ @Bind(R.id.sign_in_button) SignInButton signInButton;
+  private static final int RC_SIGN_IN = 9001;
+  private GoogleApiClient mGoogleApiClient;
+  private FirebaseAuth mFirebaseAuth;
+  private static final String TAG = "LoginActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
+
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build();
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+        .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+        .build();
+
+    // Initialize FirebaseAuth
+    mFirebaseAuth = FirebaseAuth.getInstance();
+
   }
-  
 
 
 
+  @OnClick(R.id.sign_in_button) public void signIn(){
+    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+    startActivityForResult(signInIntent, RC_SIGN_IN);
+  }
+
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+    if (requestCode == RC_SIGN_IN) {
+      GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+      if (result.isSuccess()) {
+        // Google Sign In was successful, authenticate with Firebase
+        GoogleSignInAccount account = result.getSignInAccount();
+        firebaseAuthWithGoogle(account);
+      } else {
+        // Google Sign In failed
+        Log.e(TAG, "Google Sign In failed.");
+      }
+    }
+  }
+
+
+  private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+    Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+    AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+    mFirebaseAuth.signInWithCredential(credential)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+            Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+            // If sign in fails, display a message to the user. If sign in succeeds
+            // the auth state listener will be notified and logic to handle the
+            // signed in user can be handled in the listener.
+            if (!task.isSuccessful()) {
+              Log.w(TAG, "signInWithCredential", task.getException());
+              Toast.makeText(SignInActivity.this, "Authentication failed.",
+                  Toast.LENGTH_SHORT).show();
+            } else {
+             // startActivity(new Intent(SignInActivity.this, MainActivity.class));
+              finish();
+            }
+          }
+        });
+  }
+  @Override public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+  }
 }
